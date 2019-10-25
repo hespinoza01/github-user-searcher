16b2d41ea1b79867d111bccb67e082a5ad993260
Harold Espinoza <haroldesptru@gmail.com>
2019-10-24 20:37:02 -0600

    Añadidos estilos para las tarjetas de los perfiles

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,9 +3,9 @@ import styles from './SearchBox.module.css';
 
 function SearchBox({onChange, onSubmit}) {
   return(
-    <form class={styles.searchbox} onSubmit={onSubmit}>
-      <input class={styles.searchbox__input} type="text" onChange={onChange}/>
-      <button class={styles.searchbox__submit}>Buscar</button>
+    <form className={styles.searchbox} onSubmit={onSubmit}>
+      <input className={styles.searchbox__input} type="text" onChange={onChange}/>
+      <button className={styles.searchbox__submit}>Buscar</button>
     </form>
   );
 }
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './UserCard.module.css'
 
 function UserCard({user}){
   return (
-    <article>
-      <img src={user.avatar_url} alt=""/>
-      <div>
-        <h2>{user.login}</h2>
+    <article className={styles.card}>
+      <img src={user.avatar_url} alt="" className={styles.card__img}/>
+      <div className={styles.card__infoContainer}>
+        <h3 className={styles.card__title}>{user.login}</h3>
         <p>
-          <span><a href={user.html_url} target="_blank" rel="noopener noreferrer">Ver perfil</a></span>
+          <a className={styles.card__link} href={user.html_url} target="_blank" rel="noopener noreferrer">Ver perfil</a>
         </p>
       </div>
     </article>
